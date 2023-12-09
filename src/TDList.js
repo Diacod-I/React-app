112133ce6f2ed357387715868bac50b599ff4f84
Luci <98768417+Diacod-I@users.noreply.github.com>
2023-12-09 12:39:21 +0530

    Commit initial files

---
+++ src/TDList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Component name extends Component {
+  render(){
+    return (
+      <div>
+        {These code will be rendered into the DOM}
+      </div>
+    )
+  }
+}
---
+++ src/api.py
@@ -0,0 +1,9 @@
+#api.py is an attempt of me trying to figure out how to
+#make a python flask API without documention access or YouTube
+#guidance, since I have no internet due to the Michaung Cyclone
+#(Im gonna miserably fail, but Ill try and learn)
+
+"""Approach 1: Access pip via the project terminal
+    Result: Miserably failed since pip aint recognised by
+    npm controlled terminal, dumbass.
+"""
